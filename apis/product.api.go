5fa435f341250e64726aedae4d496c16a3499ca7
nuthapon <nuthaponm79@gmail.com>
2023-10-23 20:03:06 +0700

    interceptor

--- apis/product.api.go
+++ apis/product.api.go
@@ -6,10 +6,20 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func myInterceptor(c *gin.Context) {
+	token := c.Query("token")
+	if token == "1234" {
+		c.Next()
+	} else {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+		c.Abort()
+	}
+}
+
 func SetupProductAPI(router *gin.Engine) {
 	productAPI := router.Group("api/v2")
 	{
-		productAPI.GET("/product", getProduct)
+		productAPI.GET("/product", myInterceptor, getProduct)
 		productAPI.POST("/product", createProduct)
 	}
 }
