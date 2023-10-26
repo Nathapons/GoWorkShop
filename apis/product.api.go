a77d25f18a607eab2d0c7ab767030eb75bea8cbe
Nuthapon Sripornprasert <nuthapon@swiftdynamics.co.th>
2023-10-26 16:35:10 +0700

    feat: jwt verify

--- apis/product.api.go
+++ apis/product.api.go
@@ -20,7 +20,7 @@ func SetupProductAPI(router *gin.Engine) {
 	productAPI := router.Group("api/v2")
 	{
 		productAPI.GET("/product", interceptor.JwtVerify, getProduct)
-		productAPI.POST("/product", createProduct)
+		productAPI.POST("/product", interceptor.JwtVerify, createProduct)
 	}
 }
 
