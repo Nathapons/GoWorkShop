f7d5447fc79f277b3a7f9522cf28aa8d0fd76eaa
nuthapon <nuthaponm79@gmail.com>
2023-10-28 21:16:19 +0700

    feat: left joins table with GORM

--- apis/transaction.api.go
+++ apis/transaction.api.go
@@ -18,10 +18,22 @@ func SetupTransactionAPI(router *gin.Engine) {
 	}
 }
 
+type TransactionResult struct {
+	ID uint
+	Total float64
+	Paid float64
+	Change float64
+	PaymentType string
+	PaymentDetail string
+	OrderList string
+	Staff string
+	CreatedAt time.Time
+}
+
 func getTransaction(c *gin.Context) {
-	var transaction []models.Transaction
-	db.GetDB().Find(&transaction)
-	c.JSON(http.StatusOK, gin.H{"result": transaction})
+	var result []TransactionResult
+	db.GetDB().Model(&models.Transaction{}).Select("transactions.id, transactions.total, transactions.paid, transactions.change, transactions.payment_type, transactions.payment_detail, transactions.order_list, users.username as Staff, transactions.created_at").Joins("left join users on transactions.staff_id = users.id").Scan(&result)
+	c.JSON(http.StatusOK, gin.H{"result": result})
 }
 
 func createTransaction(c *gin.Context) {
